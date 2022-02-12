a647bdaf6736b94586c19fc858c9580697feeb8b
joozoo67 <kylakim@naver.com>
2022-02-12 19:03:27 +0900

    add filterjs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import TopBar from "../src/components/TopBar";
+import Filter from "../src/components/Filter";
 
 export default function Home() {
   return (
     <div>
       <TopBar />
+      <Filter />
     </div>
   );
 }
