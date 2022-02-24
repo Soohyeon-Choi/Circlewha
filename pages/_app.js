555b4de920a89b122695198cfed3affb764fdf29
Jiyun Yun <xhrl6671@naver.com>
2022-02-25 04:45:14 +0900

    Change

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,15 @@
 import * as React from "react";
-<<<<<<< HEAD
 import Fonts from "../src/style/Fonts";
 import customTheme from "../src/style/theme";
 // 1. import `ChakraProvider` component
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-=======
-
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react";
->>>>>>> 606bb5b93d6315a8fee475de3ce01808dfc02aad
 
 export default function App({ Component }) {
   // 2. Use at the root of your app
   return (
-<<<<<<< HEAD
     <ChakraProvider theme={customTheme}>
       <Fonts />
       <Component />
     </ChakraProvider>
   );
 }
-=======
-    <ChakraProvider>
-      <Component />
-    </ChakraProvider>
-  );
-}
->>>>>>> 606bb5b93d6315a8fee475de3ce01808dfc02aad
