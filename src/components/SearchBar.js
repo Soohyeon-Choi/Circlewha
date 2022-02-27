195ab9a7c35a3cadc985d11a317be574aa8b1e70
march0320 <march02198@gmail.com>
2022-02-28 03:47:50 +0900

    Feat: search with name

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,65 +1,41 @@
-import {Input,InputGroup,Button } from "@chakra-ui/react";
-import { SearchIcon } from '@chakra-ui/icons';
+import { Input, InputGroup, Button, IconButton } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/router";
-import { useState,setState } from "react";
+import { useState, setState } from "react";
 import axios from "axios";
 import React from "react";
 
-export default function SearchBar(AddKeyword){
-    const router = useRouter();
-  const[title,setTitle]=useState('');
-  
+export default function SearchBar({ title_search, handleChange }) {
+  const pressEnter = (event) => {
+    const { key } = event;
+    if (key == "Enter") {
+      title_search();
+    }
+  };
 
-    const handleChange = ({target:{value}}) => setTitle(value); 
+  const clickButton = () => {
+    title_search();
+  };
 
-
-    const title_search =async() =>{
- 
-            axios.get('/title',
-            {params:{
-                title:`${title}`
-            }})
-            .then((res)=>{
-                console.log(res.data);
-            });
-        
-        };
-
-
-      /*  const handleEnter = (e) => {
-          if (title && e.keyCode === 13) {
-            AddKeyword(title);
-            setTitle('');
-           
-          }
-        }
-      
-        const hasTitle = !!title;
-      
-        {
-          console.log(!!title);
-        }*/
-
-
-    return(
-        <InputGroup >
-    
-        <Input variant='flushed'
-        value={title}
-        onChange={handleChange} 
-         
-        htmlSize={20} width='auto' placeholder='동아리 이름' color='#000000' focusBorderColor='#3b5735' />
-    
-    <Button rightIcon={<SearchIcon />}
-    onClick={title_search}
-    color='#3b5735' borderColor='#3b5735' variant='outline'>
-    검색
-  </Button>
-  
-  
-      </InputGroup>
-
-    );
-
-   
+  return (
+    <InputGroup>
+      <Input
+        variant="flushed"
+        onChange={handleChange}
+        onChange={(event) => handleChange(event)}
+        onKeyPress={(event) => pressEnter(event)}
+        htmlSize={20}
+        width="auto"
+        placeholder="동아리 이름 검색"
+        color="#000000"
+        focusBorderColor="#3b5735"
+      />
+      <IconButton
+        icon={<SearchIcon />}
+        onClick={clickButton}
+        color="#3b5735"
+        background="none"
+      />
+    </InputGroup>
+  );
 }
