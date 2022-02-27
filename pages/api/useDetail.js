d95969f8eceb93a68f7f87c757299f74aa66c9f8
march0320 <march02198@gmail.com>
2022-02-28 01:58:36 +0900

    Fix: property null error

--- pages/api/useDetail.js
+++ pages/api/useDetail.js
@@ -4,16 +4,37 @@ import { useState, useEffect } from "react";
 export default function useDetail(id) {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [detail, setDetail] = useState([]);
+  const [detail, setDetail] = useState({
+    act_detail:
+      "11월 셋째 주에 올리는 극을 목표로 하며, 3월부터 체계적으로 극 발제, 정기적 연습의 과정을 거칩니다. 배우팀은 일학기에는 일주일에 한 번, 이학기에는 일주일에 두 번 모여 연습을 하며 특히 여름방학의 경우 더욱 집중적인 연습 기간을 거칩니다. 스태프팀(의상소품무대팀, 홍보팀, 음향팀, 자막팀, 안무팀) 또한 일주일에 한 번씩 각 팀별 정기 회의를 가집니다. ",
+    belong_detail: "학부/전공 소속,불어불문학과",
+    etc_detail: "",
+    etccond_detail: "",
+    exinfo_detail: "#프랑스어,#원어연극,#원어뮤지컬",
+    history:
+      "https://www.youtube.com/watch?v=CQCE24Xc8v0 2006~2016 역대 공연영상 모음 참고",
+    how: "올해 2022년에는 생활환경관 소극장에서 대면 극을 올리는 것을 목표로 하고 있으며, 정기 회의 및 배우팀 연습은 거리 두기 상황에 따라 대면과 비대면을 병행할 예정입니다. ",
+    id: 59,
+    interest_detail: "예술,연극/뮤지컬",
+    link: "인스타그램 아이디 @ewha_ensemble / 유튜브 채널명: 이화불문 원어공연학회 앙상블 (https://www.youtube.com/watch?v=ofXpOMMFUsU)",
+    logo: "https://drive.google.com/uc?export=download&id=1Wbr7yskVdF78u4kQPmTLCmXc7nN3ANj_, https://drive.google.com/uc?export=download&id=1VsT7957n7lkQDQTanJd4PnB4a7bFvoOj, https://drive.google.com/uc?export=download&id=1LTHTmobUcLtdwBLsGWsbAYSQqutGHq3i",
+    period_detail: "1년 (3월~11월 셋째 주에 극이 올라갈 때까지)",
+    qual_detail: "학번,학과 무관 (휴학생도 무관)",
+    time: "투표로 결정 (배우팀의 경우 1학기 수, 2학기 수 금 예정)",
+    title: "ENSEMBLE(앙상블)",
+    when_detail: "3월 ",
+  });
 
   useEffect(() => {
     const useFetchDetail = async () => {
       setIsLoading(true);
       setIsError(false);
       try {
-        const response = await axios.get(`get-detail?num=${id}`);
+        const response = await axios.get(
+          `http://localhost:3060/get-detail?num=${id}`
+        );
         setIsLoading(false);
-        setDetail(response.data);
+        setDetail(response.data.data[0]);
       } catch (error) {
         setIsError(true);
       }
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -19,21 +19,27 @@ import useDetail from "../../pages/api/useDetail";
 
 export default function Detail({ isOpen, onOpen, onClose, id }) {
   const { detail, isError, isLoading } = useDetail(id);
-
+  console.log(detail);
+  console.log(id);
   const getField = () => {
-    var str = detail.data[0].interest_detail;
-    var str2 = detail.data[0].exinfo_detail;
+    var str = detail && detail.interest_detail;
+
     var interest = str.split(",");
     for (var i = 0; i < interest.length; i++) {
       interest[i] = "#" + interest[i];
     }
+
+    var str2 = detail && detail.exinfo_detail;
     var exinfo = str2.split(",");
+
     var field = interest.concat(exinfo);
     return field;
   };
 
   const belong = () => {
-    var bel = detail.data[0].belong_detail;
+    var bel;
+    detail.belong_detail ? (bel = detail.belong_detail) : (bel = "");
+
     var belArray = bel.split(",");
     for (var i = 0; i < belArray.length; i++) {
       belArray[i] = "#" + belArray[i];
@@ -42,230 +48,239 @@ export default function Detail({ isOpen, onOpen, onClose, id }) {
   };
 
   const etcCond = () => {
-    var etcstr = detail.data[0].etccond_detail;
+    var etcstr = detail && detail.etccond_detail;
+    if (etcstr) {
+      var cond = etcstr.split(",");
 
-    var cond = etcstr.split(",");
-    for (var i = 0; i < cond.length; i++) {
-      cond[i] = "#" + cond[i];
+      for (var i = 0; i < cond.length; i++) {
+        cond[i] = "#" + cond[i];
+      }
+    } else {
+      cond = [];
     }
     return cond;
   };
 
-  if (isLoading) {
+  // if (isLoading) {
+  //   return (
+  //     <Flex justify="center">
+  //       <Spinner
+  //         thickness="4px"
+  //         speed="0.65s"
+  //         emptyColor="gray.200"
+  //         color="blue.500"
+  //         size="xl"
+  //       />
+  //     </Flex>
+  //   );
+  // }
+
+  if (detail) {
     return (
-      <Flex justify="center">
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      </Flex>
-    );
-  }
-
-  return detail.data ? (
-    <>
-      <Modal size="xl" isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay color="#3b5735" />
-        <ModalContent>
-          <ModalCloseButton
-            borderRadius="3rem"
-            backgroundColor="darkGreen"
-            color="white"
-          />
-          <ModalBody>
-            <Heading mt={10} mb={2}>
-              {detail.data[0].title}
-            </Heading>
-            <hr />
-            <hr />
-            <Flex justify="center" mt={5} mb={10}>
-              {detail.data[0].logo == "정보없음" ? (
-                <Box
-                  borderRadius="50%"
-                  w="20rem"
-                  h="20rem"
-                  borderWidth="0.3rem"
-                  borderColor="#006540"
-                  backgroundColor="#fffecf"
-                  textAlign="center"
-                >
-                  <Text fontWeight="bold" fontSize="4xl" mt="40%">
-                    {detail.data[0].title}
-                  </Text>
-                </Box>
-              ) : (
-                <Image
-                  w="20rem"
-                  h="20rem"
-                  borderRadius="50%"
-                  src={detail.data[0].logo}
-                />
-              )}
-            </Flex>
-
-            <Flex m={1}>
-              <Flex mr={4} direction="column">
-                <Text
-                  mb={2}
-                  fontSize="1.1rem"
-                  fontWeight="bold"
-                  color="darkGreen"
-                >
-                  소속
-                </Text>
-                <Text
-                  mb={2}
-                  fontSize="1.1rem"
-                  fontWeight="bold"
-                  color="darkGreen"
-                >
-                  분야
-                </Text>
-                <Text
-                  mb={2}
-                  fontSize="1.1rem"
-                  fontWeight="bold"
-                  color="darkGreen"
-                >
-                  기타 조건
-                </Text>
+      <>
+        <Modal size="xl" isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay color="#3b5735" />
+          <ModalContent>
+            <ModalCloseButton
+              borderRadius="3rem"
+              backgroundColor="darkGreen"
+              color="white"
+            />
+            <ModalBody>
+              <Heading mt={10} mb={2}>
+                {detail.title}
+              </Heading>
+              <hr />
+              <hr />
+              <Flex justify="center" mt={5} mb={10}>
+                {detail.logo == "정보없음" ? (
+                  <Box
+                    borderRadius="50%"
+                    w="20rem"
+                    h="20rem"
+                    borderWidth="0.3rem"
+                    borderColor="#006540"
+                    backgroundColor="#fffecf"
+                    textAlign="center"
+                  >
+                    <Text fontWeight="bold" fontSize="4xl" mt="40%">
+                      {detail.title}
+                    </Text>
+                  </Box>
+                ) : (
+                  <Image
+                    w="20rem"
+                    h="20rem"
+                    borderRadius="50%"
+                    src={detail.logo}
+                  />
+                )}
               </Flex>
-              <Box m={1}>
-                <Flex>
-                  {belong().map((value, index) => (
-                    <Box
-                      mr={2}
-                      mb={2}
-                      bgColor="middleGreen"
-                      alignItems="center"
-                    >
-                      <Text key={index}>{value}</Text>
-                    </Box>
-                  ))}
+
+              <Flex m={1}>
+                <Flex mr={4} direction="column">
+                  <Text
+                    mb={2}
+                    fontSize="1.1rem"
+                    fontWeight="bold"
+                    color="darkGreen"
+                  >
+                    소속
+                  </Text>
+                  <Text
+                    mb={2}
+                    fontSize="1.1rem"
+                    fontWeight="bold"
+                    color="darkGreen"
+                  >
+                    분야
+                  </Text>
+                  <Text
+                    mb={2}
+                    fontSize="1.1rem"
+                    fontWeight="bold"
+                    color="darkGreen"
+                  >
+                    기타 조건
+                  </Text>
                 </Flex>
-                <Flex>
-                  {getField().map((value, index) => (
-                    <Flex
-                      mr={2}
-                      mb={2}
-                      bgColor="middleGreen"
-                      alignItems="center"
-                    >
-                      <Text key={index}>{value}</Text>
-                    </Flex>
-                  ))}
-                </Flex>
-                <Flex>
-                  {detail.data[0].etccond_detail == "없음" ? (
-                    <Text ml={3}>x</Text>
-                  ) : (
-                    etcCond().map((value, index) => (
-                      <Flex mr={2} bgColor="middleGreen" alignItems="center">
+                <Box m={1}>
+                  <Flex>
+                    {belong() &&
+                      belong().map((value, index) => (
+                        <Box
+                          mr={2}
+                          mb={2}
+                          bgColor="middleGreen"
+                          alignItems="center"
+                        >
+                          <Text key={index}>{value}</Text>
+                        </Box>
+                      ))}
+                  </Flex>
+                  <Flex>
+                    {getField().map((value, index) => (
+                      <Flex
+                        mr={2}
+                        mb={2}
+                        bgColor="middleGreen"
+                        alignItems="center"
+                      >
                         <Text key={index}>{value}</Text>
                       </Flex>
-                    ))
-                  )}
-                </Flex>
-              </Box>
-            </Flex>
-            <Flex m={1} mb={2}>
-              <Text
-                minW="5rem"
-                fontSize="1.1rem"
-                fontWeight="bold"
-                color="darkGreen"
-              >
-                지원 조건
-              </Text>
-              <Text ml={3}>{detail.data[0].qual_detail}</Text>
-            </Flex>
-            <Flex m={1} mb={2} alignItems="center">
-              <Text fontSize="1.1rem" fontWeight="bold" color="darkGreen">
-                필수 활동 기간
-              </Text>
-              <Text ml={3}>{detail.data[0].period_detail}</Text>
-            </Flex>
-            <Flex m={1} mb={2} alignItems="center">
-              <Text fontSize="1.1rem" fontWeight="bold" color="darkGreen">
-                활동 시간
-              </Text>
-              <Text ml={3}>{detail.data[0].time}</Text>
-            </Flex>
-            <Flex m={1} mb={2} alignItems="center">
-              <Text fontSize="1.1rem" fontWeight="bold" color="darkGreen">
-                모집 시기
-              </Text>
-              <Text ml={3}>{detail.data[0].when_detail}</Text>
-            </Flex>
+                    ))}
+                  </Flex>
+                  <Flex>
+                    {detail.etccond_detail == "없음" ? (
+                      <Text ml={3}>x</Text>
+                    ) : (
+                      etcCond().map((value, index) => (
+                        <Flex mr={2} bgColor="middleGreen" alignItems="center">
+                          <Text key={index}>{value}</Text>
+                        </Flex>
+                      ))
+                    )}
+                  </Flex>
+                </Box>
+              </Flex>
+              <Flex m={1} mb={2}>
+                <Text
+                  minW="5rem"
+                  fontSize="1.1rem"
+                  fontWeight="bold"
+                  color="darkGreen"
+                >
+                  지원 조건
+                </Text>
+                <Text ml={3}>
+                  {detail.qual_detail ? detail.qual_detail : ""}
+                </Text>
+              </Flex>
+              <Flex m={1} mb={2} alignItems="center">
+                <Text fontSize="1.1rem" fontWeight="bold" color="darkGreen">
+                  필수 활동 기간
+                </Text>
+                <Text ml={3}>{detail.period_detail}</Text>
+              </Flex>
+              <Flex m={1} mb={2} alignItems="center">
+                <Text fontSize="1.1rem" fontWeight="bold" color="darkGreen">
+                  활동 시간
+                </Text>
+                <Text ml={3}>{detail.time}</Text>
+              </Flex>
+              <Flex m={1} mb={2} alignItems="center">
+                <Text fontSize="1.1rem" fontWeight="bold" color="darkGreen">
+                  모집 시기
+                </Text>
+                <Text ml={3}>{detail.when_detail}</Text>
+              </Flex>
 
-            <Flex m={1}>
+              <Flex m={1}>
+                <Text
+                  mr={3}
+                  mb={2}
+                  fontSize="1.1rem"
+                  fontWeight="bold"
+                  color="darkGreen"
+                  alignItems="center"
+                >
+                  SNS
+                </Text>
+                {detail.link}
+              </Flex>
+
               <Text
-                mr={3}
-                mb={2}
+                m={1}
+                mt={7}
                 fontSize="1.1rem"
                 fontWeight="bold"
                 color="darkGreen"
                 alignItems="center"
               >
-                SNS
+                활동 내용
               </Text>
-              {detail.data[0].link}
-            </Flex>
-
-            <Text
-              m={1}
-              mt={7}
-              fontSize="1.1rem"
-              fontWeight="bold"
-              color="darkGreen"
-              alignItems="center"
-            >
-              활동 내용
-            </Text>
-            <Text m={1}>{detail.data[0].act_detail}</Text>
-            <Text
-              m={1}
-              mt={7}
-              fontSize="1.1rem"
-              fontWeight="bold"
-              color="darkGreen"
-              alignItems="center"
-            >
-              활동 방식
-            </Text>
-            <Text m={1}>{detail.data[0].how}</Text>
-            <Text
-              m={1}
-              mt={7}
-              fontSize="1.1rem"
-              fontWeight="bold"
-              color="darkGreen"
-              alignItems="center"
-            >
-              동아리 연혁
-            </Text>
-            <Text m={1}>{detail.data[0].history}</Text>
-            <Text
-              m={1}
-              mt={7}
-              fontSize="1.1rem"
-              fontWeight="bold"
-              color="darkGreen"
-              alignItems="center"
-            >
-              기타 참고 사항
-            </Text>
-            <Text m={1} mb={10}>
-              {detail.data[0].etc_detail}
-            </Text>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
-  ) : (
-    ""
-  );
+              <Text m={1}>{detail.act_detail}</Text>
+              <Text
+                m={1}
+                mt={7}
+                fontSize="1.1rem"
+                fontWeight="bold"
+                color="darkGreen"
+                alignItems="center"
+              >
+                활동 방식
+              </Text>
+              <Text m={1}>{detail.how}</Text>
+              <Text
+                m={1}
+                mt={7}
+                fontSize="1.1rem"
+                fontWeight="bold"
+                color="darkGreen"
+                alignItems="center"
+              >
+                동아리 연혁
+              </Text>
+              <Text m={1}>{detail.history ? detail.history : "x"}</Text>
+              <Text
+                m={1}
+                mt={7}
+                fontSize="1.1rem"
+                fontWeight="bold"
+                color="darkGreen"
+                alignItems="center"
+              >
+                기타 참고 사항
+              </Text>
+              <Text m={1} mb={10}>
+                {detail.etc_detail ? detail.etc_detail : "x"}
+              </Text>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  } else {
+    return <></>;
+  }
 }
